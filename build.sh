4da560c1b9ea8808baa9e81ba6ea46090a974843
Hai Lu <hai.lu@c-mg.com>
2015-03-07 11:23:18 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
-cd /Volumes/DATA/Development/radiko/rtmpdump-android/app/src/main
+WORKING_DIR=`pwd`
+# build RTMPDump
+cd $WORKING_DIR/rtmpdump-android/app/src/main
 ndk-build
-cp -R /Volumes/DATA/Development/radiko/rtmpdump-android/app/src/main/libs/. /Volumes/DATA/Development/radiko/radiko/app/src/main/jniLibs
+cp -R $WORKING_DIR/rtmpdump-android/app/src/main/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+# build FFmpeg MediaPlayer
+# If it the first time, need to compile openssl & ffmpeg first.
+# Read fmp-library/README.md
+
+cd $WORKING_DIR/fmp-library
+ndk-build
+cp -R $WORKING_DIR/fmp-library/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+# build liblame
+cd $WORKING_DIR/liblame
+ndk-build
+cp -R $WORKING_DIR/liblame/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+#build libautotalent
+cd $WORKING_DIR/libautotalent
+ndk-build
+cp -R $WORKING_DIR/libautotalent/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
