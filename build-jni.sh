34577e0570e8c5772d6f97f2608e5930391cfb86
Hai Lu <hai.lu@c-mg.com>
2015-03-07 11:25:53 +0700

    Update JNI build

---
+++ build-jni.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+WORKING_DIR=`pwd`
+# build RTMPDump
+cd $WORKING_DIR/rtmpdump-android/app/src/main
+ndk-build
+cp -R $WORKING_DIR/rtmpdump-android/app/src/main/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+# build FFmpeg MediaPlayer
+# If it the first time, need to compile openssl & ffmpeg first.
+# Read fmp-library/README.md
+
+cd $WORKING_DIR/fmp-library
+ndk-build
+cp -R $WORKING_DIR/fmp-library/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+# build liblame
+cd $WORKING_DIR/liblame
+ndk-build
+cp -R $WORKING_DIR/liblame/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
+
+#build libautotalent
+cd $WORKING_DIR/libautotalent
+ndk-build
+cp -R $WORKING_DIR/libautotalent/libs/. $WORKING_DIR/radiko/app/src/main/jniLibs
